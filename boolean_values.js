fbe2a5abe94ad201dd3f19fc9a65eb7b81e31b29
edjose2206 <edjose2206@gmail.com>
2021-03-12 20:11:18 -0600

    Update boolean_values.js
    Adding more code about the boolean data type

--- boolean_values.js
+++ boolean_values.js
@@ -16,7 +16,78 @@ function trueOrFalse(wasThatTrue) {
     if (wasThatTrue){
         return "Yes, that was true";
     }
-    return "No, that was flase";
+    return "No, that was false";
 }
 
 console.log(trueOrFalse(true));
+
+// Comparison with the Equality Operator
+// Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+// Change this value to test
+console.log(testEqual(10));
+
+// Comparison with the Scrict Equality Operator
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+// == use a convert funtion to make the comparation
+// === doen't use the convertion function 
+/*
+3 === 3 => True
+3 === '3' => False
+ */
+
+// Change this value to test
+console.log(testStrict(10));
+
+// Practice Comparing Different Values
+// Setup
+function compareEquallity(a,b) {
+    if(a == b){ // Change this line
+        return "Equal";
+    }
+    return "Not Equal"
+}
+
+// Change this value to test
+console.log(compareEquallity(10,"10"));
+
+// Comparison with the Inequality Operator
+// Setup
+function testNotEqual(val) {
+    if (val!=22) { //Change this line FYI != type convertion
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+// Change this value to test
+console.log(testNotEqual(10));
+
+// Comparison with the Strict Inequality Operator
+// Setup
+function testStrictNotEqual(val) {
+    // Only Change Code Below this Line
+    if (val !== 22) {
+        // Only Change Code Above this Line
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+// Change this value to test
+console.log(testStrictNotEqual(10));
+
+// Comparison with the Logical AND Operator -> 1:17:08
